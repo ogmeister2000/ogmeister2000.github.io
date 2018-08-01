9bef9ee4b6f25a51353d8f9c62d9f5bf6c0cb743
Olivia Meister <ogmeister2000@gmail.com>
2018-08-01 11:33:48 -0700

    trash tv

---
+++ myScript.js
@@ -0,0 +1,7 @@
+
+function pictureChange()
+    {
+          document.getElementById("theImage").src="http://cdn01.cdn.justjared.com/wp-content/uploads/headlines/2015/05/kardashians-react-bruce-jenner-special2.jpg";
+    }
+
+  
