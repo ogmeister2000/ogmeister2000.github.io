7e10d22b42e91e1fe84bf572165a08fb6a6bde77
Olivia Meister <ogmeister2000@gmail.com>
2018-08-01 13:02:13 -0700

    blegh

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ function pictureChange()
           document.getElementById("theImage").src="http://cdn01.cdn.justjared.com/wp-content/uploads/headlines/2015/05/kardashians-react-bruce-jenner-special2.jpg";
     }
 
-    function pictureChange2()
-        {
+function pictureChange2()
+    {
               document.getElementById("theImage").src="https://i.dailymail.co.uk/i/pix/2018/02/16/05/4942AF7900000578-5397981-image-m-96_1518760135508.jpg";
-        }
+    }
